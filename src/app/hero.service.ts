de6c58327dea3eceb88391cfbe3a927f1f1f8657
Carroll, Ryan <CARROR6@nationwide.com>
2018-01-29 16:38:58 -0500

    adding hero detail features from tutorial

---
+++ src/app/hero.service.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class HeroService {
+
+  constructor() { }
+
+  getHeroes(): Hero[] {
+    return HEROES;
+  }
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+
+
+import { Hero } from './hero';
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Captain America' },
+  { id: 12, name: 'Deadpool' },
+  { id: 13, name: 'Spiderman' },
+  { id: 14, name: 'Thor' },
+  { id: 15, name: 'Power Man' },
+  { id: 16, name: 'Iron Fist' }
+];
